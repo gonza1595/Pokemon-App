8d80060422ac32d5054ed3d0c613accc451489ce
gonza1595 <aveirogonzalo3cnp@gmail.com>
2023-05-03 16:58:16 -0300

    index/route/getTypes

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { getTypes } = require("../api/src/routes/Controllers");
+const { getTypes } = require("./src/routes/index.js");
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
